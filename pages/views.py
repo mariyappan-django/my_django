d60537248789aa52e7ca4333c3089c0fef2b1e25
mariyappan-django <ios.mariyappan@gmail.com>
2020-04-12 21:57:21 +0530

    first commit on 12-04-2020

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 # def homePageView(request):
-#
 #     return HttpResponse('welcome to pages!')
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
